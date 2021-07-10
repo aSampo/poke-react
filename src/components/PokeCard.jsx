fbc9e4284a9804583dc9b487b346c171ff7fee8b
Andres Sampo <andressampo@gmail.com>
2021-07-10 15:19:52 -0300

    request refactor

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,38 +1,7 @@
 import React from 'react';
-import { Box, Text, Image, Center, Flex } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { Box, Text, Image, Center } from '@chakra-ui/react';
 
-function PokeCard({ url }) {
-  const [pokemon, setPokemon] = useState({});
-  const [status, setStatus] = useState('init');
-
-  useEffect(() => {
-    setStatus('pending');
-    axios
-      .get(url)
-      .then(res => {
-        setPokemon(res.data);
-        setStatus('resolved');
-      })
-      .catch(error => {
-        console.log(error);
-        setStatus('rejected')
-      });
-  }, [url]);
-
-  if (status !== 'resolved') {
-    return (
-      <Box
-        m="15"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        height="200px"
-        textAlign="center"
-      />
-    );
-  }
+function PokeCard({ pokemon }) {
 
   return (
     <Box
@@ -49,9 +18,9 @@ function PokeCard({ url }) {
       <Text fontSize="2xl">
         {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
       </Text>
-      {pokemon.types.map( type => <Text key={type.type.name}>{type.type.name}</Text>)}
-      
-      
+      {pokemon.types.map(type => (
+        <Text key={type.type.name}>{type.type.name}</Text>
+      ))}
     </Box>
   );
 }
